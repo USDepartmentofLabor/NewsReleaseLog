17e1641a4b5ebd971c196ece885afc7e069a2e85
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-06-29 09:50:10 -0400

    Adding formtastic for forms

--- app/policies/news_log_policy.rb
+++ app/policies/news_log_policy.rb
@@ -1,4 +1,14 @@
 class NewsLogPolicy < ApplicationPolicy
+  attr_reader :user, :news_log
+
+  def initialize(user, news_log)
+    @user = user
+    @news_log = news_log
+  end
+
+  def update?
+    user.admin? or not news_log.published?
+  end
 
   def create?
     user.admin?
