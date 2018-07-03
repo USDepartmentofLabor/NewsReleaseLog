0d83b31db25688bdd75933cad13f297dd939b00f
Hari Yerramsetty <yhvardhan@gmail.com>
2018-07-03 08:28:40 -0400

    Adding mongoid-history to keep track of all the actions on newsreleaselog

--- app/models/news_log.rb
+++ app/models/news_log.rb
@@ -2,17 +2,29 @@ class NewsLog
   include Mongoid::Document
   include Mongoid::Timestamps
   include AASM
+  include Mongoid::History::Trackable
+
   # Fields
   field :received_date, type: Time
   field :release_date, type: Time
   field :title, type: String
   field :news_release_number, type: String
   field :aasm_state
+
   # Associations
   belongs_to :user
   belongs_to :agency, optional: true
   belongs_to :region, optional: true
   has_and_belongs_to_many :distributionlists
+
+  track_history   :on => [:title, :received_date, :release_date, :aasm_state],
+                  :modifier_field => :modifier,
+                  :modifier_field_inverse_of => :nil,
+                  :modifier_field_optional => true,
+                  :version_field => :version,
+                  :track_create  => true,
+                  :track_update  => true,
+                  :track_destroy => true
 
   scope :published, -> { where(aasm_state: 'published') }
   scope :drafts, -> { where(aasm_state: 'draft') }
