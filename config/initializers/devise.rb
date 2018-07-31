00228af2fd270e049d267219deeb1c43e0d5ab08
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-07-31 13:58:02 -0400

    Enabling devise secret for prod env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,8 +8,8 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '31e79d62126af7d474a7e98f637cd2788955f561a9e5bc96855021ad11a47b4aaa12408e0192fd5c053668a0f272a1efa21d9e30ab844bb38f514b8e8947f66c'
-  
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
