781860a1eeb41eabb865ba177e6ab77b14647666
Hari Yerramsetty <yhvardhan@gmail.com>
2018-08-07 13:38:01 -0400

    UI changes for history page

--- app/models/history_tracker.rb
+++ app/models/history_tracker.rb
@@ -6,18 +6,22 @@ class HistoryTracker
     if self.original.present?
       self.modified.keys.each do |key|
         case key
+        when 'title'
+          changes["Title"] = { changed: self.modified[key].capitalize, original: self.original[key].capitalize }
         when 'received_date'
           changes["Received Date"] = { changed: self.modified[key], original: self.original[key] }
         when 'release_date'
           changes["Release Date"] = { changed: self.modified[key], original: self.original[key] }
         when 'agency_id'
-          changes["Agency"] = { changed: get_agency_name(self.modified[key]), original: get_agency_name(self.original[key]) }
+          changes["Agency"] = { changed: get_agency_name(self.modified[key]).upcase, original: get_agency_name(self.original[key]).upcase }
         when 'region_id'
-          changes["Region"] = { changed: get_region_name(self.modified[key]), original: get_region_name(self.original[key]) }
+          changes["Region"] = { changed: get_region_name(self.modified[key]).capitalize, original: get_region_name(self.original[key]).capitalize }
         when 'distributionlist_ids'
           changes["Distribution Lists"] = { changed: get_distribution_lists(self.modified[key]), original: get_distribution_lists(self.original[key]) }
         when 'aasm_state'
-          changes['State'] = { changed: self.modified[key], original: self.original[key] }
+          changes['State'] = { changed: self.modified[key].capitalize, original: self.original[key].capitalize }
+        when 'document'
+          changes['Document'] = { changed: self.modified[key] }
         end
       end
       changes
@@ -28,6 +32,8 @@ class HistoryTracker
     changes = {}
     self.modified.keys.each do |key|
       case key
+      when 'title'
+        changes['Title'] = { created: self.modified[key] }
       when 'news_release_number'
         changes['News Release Number'] = { created: self.modified[key] }
       when 'received_date'
@@ -39,7 +45,7 @@ class HistoryTracker
       when 'region_id'
         changes["Region"] = { created: get_region_name(self.modified[key]) }
       when 'aasm_state'
-        changes["State"] = { created: self.modified[key] }
+        changes["State"] = { created: self.modified[key].capitalize }
       when 'distributionlist_ids'
         changes["Distribution Lists"] = { created: get_distribution_lists(self.modified[key]) }
       when 'document'
