82aca65e01a1b57c0c5053669091bf6463dad77c
sarathyerra14 <yerra.sarath.p@dol.gov>
2018-08-30 14:40:43 -0400

    matching field names

--- script/import_nrl_data.rb
+++ script/import_nrl_data.rb
@@ -20,6 +20,13 @@
 csv_text = File.read(Rails.root.join('dump', '.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
+  region = Region.find_by_name(row['RegionName'])
+  agency = Agnecy.find_by_name(row['AgencyName'])
+  unless region || agency
+  	puts "Unable to find region or agency .. RegionName #{row[RegionName]} Agnecy #{row[AgencyName]}" 
+  	puts "Skipping OPAID #{row['OPAID']}"
+  	next
+  end
   d = NewsLog.new
   d.opa_id = row['OPAID']
   d.title = row["title"]
@@ -27,9 +34,9 @@ csv.each do |row|
   d.release_date = row["ReleaseDate"]
   d.news_release_number = row["OPASeqnum2"]
   d.aasm_state ="published"
-  Region.find_by_code(row[''])
-  d.region =row['RegionID']
-  Agnecy.find_by_code(row[''])
-  d.name=row['AgencyID']
-  d.save
+  d.agency = agency
+  d.region = region
+  if d.save
+  	puts "Successfully saved #{row['OPAID']}"
+  end
 end
