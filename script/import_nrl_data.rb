93a842c621e4b1a41043f0f87bf754925e07310a
sarathyerra14 <yerra.sarath.p@dol.gov>
2018-09-06 14:02:21 -0400

    Refs #data transfer to new newsrelease log

--- script/import_nrl_data.rb
+++ script/import_nrl_data.rb
@@ -1,25 +1,34 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('dump', 'OPARelease_with_name.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv_text = File.open(Rails.root.join('dump', 'OPARelease_with_name.csv'),"r:ISO-8859-1")
+csv = CSV.parse(csv_text, :headers => true)
+
 csv.each do |row|
-  region = Region.find_by_name(row['RegionName'])
-  agency = Agnecy.find_by_name(row['AgencyName'])
-  unless region || agency
-  	puts "Unable to find region or agency .. RegionName #{row[RegionName]} Agnecy #{row[AgencyName]}" 
+  region = Region.where(:name => /^#{row['RegionName'].strip}$/i).first
+  agency = Agency.where(:name => /^#{row['AgencyName'].strip}$/i).first
+  if !region.present?
+    puts "Unable to find region .. RegionName #{row['RegionName']} Agnecy #{row['AgencyName']}" 
+    puts "Skipping OPAID #{row['OPAID']}"
+    next
+  elsif !agency.present?
+  	puts "Unable to find agency .. Agnraoecy #{row['AgencyName']}" 
   	puts "Skipping OPAID #{row['OPAID']}"
   	next
   end
+  puts "OPASeqnum2 #{row['OPASeqnum2']}"
   d = NewsLog.new
   d.opa_id = row['OPAID']
-  d.title = row["title"]
+  d.title = row["Title"]
   d.received_date = row["ReceivedDate"]
   d.release_date = row["ReleaseDate"]
   d.news_release_number = row["OPASeqnum2"]
   d.aasm_state ="published"
-  d.agency = agency
-  d.region = region
+  d.agency = agency.id
+  d.region = region.id
+  d.user = User.first
   if d.save
   	puts "Successfully saved #{row['OPAID']}"
+  else
+    puts "Unable to save #{d.errors.full_messages}"
   end
 end
