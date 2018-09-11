d76f9a42b3a8de19d0653afe1191121ec64e0ea4
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-09-11 13:00:38 -0400

    fixed variable name for reg search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search
     unless params[:search].blank?
       results = NewsLog.search(params[:search])
-      @sorted_results = Kaminari.paginate_array(results).page(params[:page]).per(10)
+      @page_results = Kaminari.paginate_array(results).page(params[:page]).per(10)
     else
       redirect_to root_path, alert: "Please enter a valid Title or NewsRelease number"
     end
