8ff7d2a9bd7a129aa7bad23f9a979866491665dd
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-09-18 11:07:53 -0400

    adding new column frequently_used

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -3,12 +3,13 @@ class Agency
   include Mongoid::Timestamps
   field :name, type: String
   field :code, type: String
+  field :frequently_used, type: Boolean
   validates :name, presence: true, uniqueness: true
   validates :code, presence: true, uniqueness: true
   before_validation :capitalize_attributes
 
   def capitalize_attributes
-    self.name = self.name.capitalize
+    self.name = self.name.upcase
     self.code = self.code.upcase
   end
 end
