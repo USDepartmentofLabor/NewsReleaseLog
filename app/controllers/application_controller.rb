bba7114e52621e8e5975ae2a3da9110ebe7af6e5
Giridhar Bandi <bandi.giridhar.r@dol.gov>
2018-10-25 12:25:18 -0400

    Authentication token issue in production mode while downloading csv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'CSV' }
+
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
